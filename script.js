bb3c57e2c58add91bd2e754fd59f53825a3edee1
Micheil <fouriemicheil1@gmail.com>
2024-08-15 14:32:33 +0200

    finished js commit

--- script.js
+++ script.js
@@ -108,19 +108,93 @@ class ShoppingCart {
         this.taxRate = 8.25;
     }
 
+
+
     addItem(id, products) {
 
         const product = products.find((item) => item.id === id);
         const { name, price } = product;
-
         this.items.push(product);
         const totalCountPerProduct = {};
 
         this.items.forEach((dessert) => {
 
-            totalCountPerProduct[dessert.id] = totalCountPerProduct[dessert.id] + 1;
-
+            totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
 
         });
+
+        const currentProductCount = totalCountPerProduct[product.id];
+        const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`);
+
+        const isProductInCart =
+            currentProductCount > 1
+                ? currentProductCountSpan.textContent = `${currentProductCount}x`
+                : productsContainer.innerHTML +=
+                `
+                <div class="product" id="dessert${id}">
+                    <p><span class="product-count" id="product-count-for-id${id}"></span>${name}</p>
+                    <p>${price}</p>
+                </div>
+                `;
     }
+
+    getCounts() {
+        return this.items.length;
+    }
+
+    calculateTotal() {
+        const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+        const tax = this.calculateTaxes(subTotal);
+        this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
+        return this.total;
+    }
+
+    calculateTaxes(amount) {
+        return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+
+    clearCart() {
+        if (!this.items.length) {
+            alert("Your shopping cart is already empty");
+            return;
+        }
+
+        const isCartCleared = confirm("Are you sure you want to clear all items from your shopping cart?")
+
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+            productsContainer.innerHTML = "";
+            cartSubTotal.textContent = 0;
+            cartTaxes.textContent = 0;
+            cartTotal.textContent = 0;
+            totalNumberOfItems.textContent = 0;
+        }
+
+    }
+
 };
+
+const cart = new ShoppingCart();
+const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+const arr = [...addToCartBtns].forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        cart.addItem(Number(event.target.id), products);
+        totalNumberOfItems.textContent = cart.getCounts();
+        cart.calculateTotal();
+    })
+});
+
+cartBtn.addEventListener("click", () => {
+
+    isCartShowing = !isCartShowing;
+    showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+    cartContainer.style.display = isCartShowing ? "block" : "none";
+
+});
+
+clearCartBtn.addEventListener("click", cart.clearCart.bind(cart));
